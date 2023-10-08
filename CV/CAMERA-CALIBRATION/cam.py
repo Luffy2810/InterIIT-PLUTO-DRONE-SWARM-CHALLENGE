e0b1acdbe2e727b031a17af2da3402862c4d01c9
Sahil Sudhakar <96567974+Luffy2810@users.noreply.github.com>
2023-10-08 14:08:28 +0530

    First Commit

---
+++ CV/CAMERA-CALIBRATION/cam.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+import numpy as np
+class Camera:
+    def __init__(self,width=1920,height=1080):
+        self.res_width=width
+        self.res_height=height
+        self.cap = cv.VideoCapture('/dev/video2')
+
+        
+        if self.res_width!=None and self.res_height!=None:
+            self.cap.set(cv.CAP_PROP_FRAME_WIDTH, self.res_width)
+            self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, self.res_height)
+            codec = cv.VideoWriter_fourcc(*'MJPG')
+            self.cap.set(cv.CAP_PROP_FOURCC, codec)
+            self.cap.set(cv.CAP_PROP_FPS, 20)
+    def get_camera_frame(self):
+        ret, frame = self.cap.read()
+        print (frame.shape)
+        return frame
+    def end_camera_feed(self):
+        self.cap.release()
+        cv.destroyAllWindows()
+Cam=Camera()
+while True:
+    frame=Cam.get_camera_frame()
+    cv.imshow("frame", frame)
+    key = cv.waitKey(1)
+    if key == ord("q"):
+        break
+Cam.end_camera_feed()
